606637c97eec3c32582404d6350c31e03de2e55d
jorge <jalbert400@gmail.com>
2023-06-25 12:31:35 -0500

    subiendo codigo de views

--- scraping/views.py
+++ scraping/views.py
@@ -3,17 +3,33 @@ from .forms import DeeplinkForm
 from scraping.validations import validacionInput
 
 def webscraping(request):
-	#return render(request, 'pages/index.html')
-
     msnEstado = False
     msnError = ''
     msnResp = ''
     msnNOEXResp = ''
     msnNOVALResp = ''
     msnVal = []
-    if 1 == 1:
-        print('aaaa')
+    if request.method == "POST":
+        form = DeeplinkForm(request.POST)
+        if form.is_valid():
+            msnEstado = True
+            idCategoria = request.POST['idcategoria']
+            tipoNavegador = request.POST['navegador'] 
+            msnVal = validacionInput(idCategoria,tipoNavegador)
+            if (len(msnVal[2]) > 1):
+                if msnVal[2][1] == 'validation-field':
+                    msnNOVALResp = msnVal[2][0]
+            if (len(msnVal[1]) > 1):
+                if msnVal[1][1] == 'no-exit-field':
+                    msnNOEXResp = msnVal[1][0]
+            if (len(msnVal[0]) > 1):
+                if msnVal[0][1] == 'error-field':
+                    msnError = msnVal[0][0]
+                if msnVal[0][1] == 'exit-field':
+                    msnResp = msnVal[0][0]
+            redirect('/')
     else:
-        print('bbbbbb')
+        form = DeeplinkForm()
         
+	#return render(request,'pages/index.html',{'form':DeeplinkForm,'msnEstado':msnEstado,'msnError':msnError,'msnResp':msnResp,'msnNOEXResp':msnNOEXResp,'msnNOVALResp':msnNOVALResp})
     return render(request, 'pages/index.html')

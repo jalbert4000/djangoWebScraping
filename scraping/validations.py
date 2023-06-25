ee5b7a0296882d17a3b4ec1a12424c4f43bdb664
jorge <jalbert400@gmail.com>
2023-06-25 12:05:16 -0500

    agregando file web

--- scraping/validations.py
+++ scraping/validations.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-#from scraping.webScraping import WebScraping
+from scraping.webScraping import WebScraping
 #import re
 
 def validacionInput(idCategoria,tipoNavegador):

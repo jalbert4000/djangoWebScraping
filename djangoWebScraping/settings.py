69317034bd0bcc080cfb0081ed42bd0eb8df9397
jorge <jalbert400@gmail.com>
2023-06-25 05:07:24 -0500

    quitanto base

--- djangoWebScraping/settings.py
+++ djangoWebScraping/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 #STATIC_URL = 'static/'
-STATIC_URL = 'scraping/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

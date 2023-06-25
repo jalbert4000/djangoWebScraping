b574ad76dc1b3e4e4fa6efb5259e34168224a738
jorge <jalbert400@gmail.com>
2023-06-25 04:51:04 -0500

    subiendo carpetas

--- scraping/views.py
+++ scraping/views.py
@@ -2,4 +2,17 @@ from django.shortcuts import render, redirect
 from .forms import DeeplinkForm 
 
 def webscraping(request):
-	return render(request, 'pages/index.html')
+	#return render(request, 'pages/index.html')
+
+    msnEstado = False
+    msnError = ''
+    msnResp = ''
+    msnNOEXResp = ''
+    msnNOVALResp = ''
+    msnVal = []
+    if 1 == 1:
+        print('aaaa')
+    else:
+        print('bbbbbb')
+        
+    return render(request, 'pages/index.html')

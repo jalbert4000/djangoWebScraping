5be48e09894285c3e6205a463c0160900633ebe6
jorge <jalbert400@gmail.com>
2023-06-26 01:23:06 -0500

    subiendo otro urls

--- djangoWebScraping/urls.py
+++ djangoWebScraping/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 #from djangoWebScraping.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('inicio', inicio),
     path('',include('scraping.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

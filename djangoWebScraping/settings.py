6e77fc974306e7ff593e13456656fb6df252f138
jorge <jalbert400@gmail.com>
2023-06-25 20:00:50 -0500

    subiendo estilos

--- djangoWebScraping/settings.py
+++ djangoWebScraping/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 #STATIC_URL = 'static/'
 STATIC_URL = 'scraping/static/'
+BASE_URL="https://django-web-scraping.vercel.app"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

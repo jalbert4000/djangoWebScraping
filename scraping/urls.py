83dee4db740c9651f0104d5994d28d1affab05b3
jorge <jalbert400@gmail.com>
2023-06-26 01:59:50 -0500

    subiendo urls

--- scraping/urls.py
+++ scraping/urls.py
@@ -5,4 +5,4 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',webscraping)
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

d24075204c1be6f7b7027a166b8150425591ded3
jorge <jalbert400@gmail.com>
2023-06-26 04:03:43 -0500

    subiendo cambios de settings

--- djangoWebScraping/urls.py
+++ djangoWebScraping/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-#from django.conf import settings
-#from django.conf.urls.static import static
 #from djangoWebScraping.views import inicio
 
 urlpatterns = [
@@ -9,6 +7,3 @@ urlpatterns = [
     #path('inicio', inicio),
     path('',include('scraping.urls'))
 ]
-
-#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
